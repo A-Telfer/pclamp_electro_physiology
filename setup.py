b49a2ae2256234421c6f6025a88ddd1b9d42354d
Andre Telfer <andretelfer@cmail.carleton.ca>
2023-06-06 19:55:23 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pclamp_electro_physiology_classification',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Filtering noisy from clean ephys signals',
+    author='Andre Telfer',
+    license='MIT',
+)
